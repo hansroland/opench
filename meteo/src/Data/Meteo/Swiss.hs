33db11213e70f1106866fd6f5dc5160cfbefee61
roland <rsx@bluewin.ch>
2017-03-22 10:31:34 +0100

    Add documentation for haddock

--- meteo/src/Data/Meteo/Swiss.hs
+++ meteo/src/Data/Meteo/Swiss.hs
@@ -1,3 +1,14 @@
-module Data.Meteo.Swiss (module X) where
+-- | A Haskell implementation of the Swiss Meteo Net data API
+--
+-- OpenData SMN is a REST API for <http://www.meteoswiss.admin.ch/home/measurement-and-forecasting-systems/land-based-stations/automatisches-messnetz.html SwissMetNet > data.
+-- It's developed in the open supporting the <http://opendata.ch/ OpenData.ch > initiative. 
+-- Free OpenData hosting is provided by <http://netcetera.com/ Netcetera > at <http://data.netcetera.com/smn >.
+--
+-- The actual data for 10 min intervals is provided by opendata.swiss, the Swiss open government data portal.
 
-import Data.Meteo.Swiss.Types as X
+module Data.Meteo.Swiss 
+  (
+    module Data.Meteo.Swiss.Types
+  ) 
+  where
+import Data.Meteo.Swiss.Types
--- meteo/src/Data/Meteo/Swiss/Types.hs
+++ meteo/src/Data/Meteo/Swiss/Types.hs
@@ -1,6 +1,12 @@
  {-# LANGUAGE OverloadedStrings #-}
- 
-module Data.Meteo.Swiss.Types where
+
+-- | Data types for the Swiss Meteo Net API
+module Data.Meteo.Swiss.Types
+ (
+ -- * Types
+   SmnRecord (..)
+ , SmnStation(..)
+ ) where
 
 import           Data.Aeson
 import qualified Data.Text as T
@@ -9,25 +15,26 @@ import           Data.Aeson.Types (Parser)
 import           Data.String (IsString)
 import           Control.Monad (mzero)
 
-import qualified Data.ByteString.Lazy as B
-
+-- | A Swiss Meteo Network data SmnRecord.
+-- It contains the meteo information of a 10 min interval of a Swis meteo station.
+-- 
+-- /Note:/ All 'Text' fields may contain empty strings.
 data SmnRecord = SmnRecord 
-  { smnStation       :: Maybe Station -- optional  Info about the station
-  , smnCode          :: T.Text        -- optional: 3-char all upper-case station code,
-  , smnDateTime      :: Maybe UTCTime -- optional: Time in UTC,
-  , smnTemperature   :: T.Text        -- optional: Air temperature 2 m above ground; current value,
-  , smnSunshine      :: T.Text        -- optional: Sunshine duration; ten minutes total,
-  , smnPrecipitation :: T.Text        -- optional: Precipitation; ten minutes total,
-  , smnWindDirection :: T.Text        -- optional: Wind direction; ten minutes mean,
-  , smnWindSpeed     :: T.Text        -- optional: Wind speed; ten minutes mean,
-  , smnQnhPressure   :: T.Text        -- optional: Pressure reduced to sea level according to standard atmosphere (QNH); current value,
-  , smnGustPeak      :: T.Text        -- optional: Gust peak (one second); maximum,
-  , smnHumidity      :: T.Text        -- optional: Relative air humidity 2 m above ground; current value,
-  , smnQfePressure   :: T.Text        -- optional: Pressure at station level (QFE); current value,
-  , smnQffPressure   :: T.Text        -- optional: Pressure reduced to sea level (QFF); current value
+  { smnStation       :: Maybe SmnStation   -- ^  Info about the station
+  , smnCode          :: T.Text             -- ^ 3-char all upper-case station code,
+  , smnDateTime      :: Maybe UTCTime      -- ^ Time in UTC,
+  , smnTemperature   :: T.Text             -- ^ Air temperature 2 m above ground; current value,
+  , smnSunshine      :: T.Text             -- ^ Sunshine duration; ten minutes total,
+  , smnPrecipitation :: T.Text             -- ^ Precipitation; ten minutes total,
+  , smnWindDirection :: T.Text             -- ^ Wind direction; ten minutes mean,
+  , smnWindSpeed     :: T.Text             -- ^ Wind speed; ten minutes mean,
+  , smnQnhPressure   :: T.Text             -- ^ Pressure reduced to sea level according to standard atmosphere (QNH); current value,
+  , smnGustPeak      :: T.Text             -- ^ Gust peak (one second); maximum,
+  , smnHumidity      :: T.Text             -- ^ Relative air humidity 2 m above ground; current value,
+  , smnQfePressure   :: T.Text             -- ^ Pressure at station level (QFE); current value,
+  , smnQffPressure   :: T.Text             -- ^ Pressure reduced to sea level (QFF); current value
   }
  deriving Show
-
 
 instance FromJSON SmnRecord where 
   parseJSON (Object v) = SmnRecord
@@ -46,20 +53,22 @@ instance FromJSON SmnRecord where
     <*> v .:?! "qffPressure"
   parseJSON _ = mzero
 
-
-data Station = Station 
-  { staCode          :: T.Text        -- optional: 3-char all upper-case station code,
-  , staName          :: T.Text        -- optional: Original name in local language,
-  , staCh1903Y       :: Maybe Int     -- optional: CH1903 (Swiss grid) y-axis value,
-  , staCh1903X       :: Maybe Int     -- optional: CH1903 (Swiss grid) x-axis value,
-  , staLat           :: Maybe Double  -- optional: WGS84 latitude,
-  , staLng           :: Maybe Double  -- optional: WGS84 longitude,
-  , staElevation     :: Maybe Int     -- optional: meters above sea level
+-- | A Swiss Meteo Network data station.
+--
+-- /Note:/ All 'Text' fields may contain empty strings.
+data SmnStation = SmnStation 
+  { staCode          :: T.Text         -- ^ 3-char all upper-case station code,
+  , staName          :: T.Text         -- ^  Original name in local language,
+  , staCh1903Y       :: Maybe Int      -- ^  CH1903 (Swiss grid) y-axis value,
+  , staCh1903X       :: Maybe Int      -- ^  CH1903 (Swiss grid) x-axis value,
+  , staLat           :: Maybe Double   -- ^  WGS84 latitude,
+  , staLng           :: Maybe Double   -- ^  WGS84 longitude,
+  , staElevation     :: Maybe Int      -- ^  meters above sea level
   }
  deriving Show
 
-instance FromJSON Station where 
-  parseJSON (Object v) = Station
+instance FromJSON SmnStation where 
+  parseJSON (Object v) = SmnStation
     <$> v .:?! "code"
     <*> v .:?! "name"
     <*> v .:? "ch1903Y"
@@ -69,34 +78,6 @@ instance FromJSON Station where
     <*> v .:? "elevation"
   parseJSON _ = mzero
  
-  -- | little helper combinator to parse and convert null values to empty test strings.
+-- little helper combinator to parse and convert null values to empty Text strings.
 (.:?!) :: (Data.String.IsString a, FromJSON a) => Object -> T.Text -> Parser a
 (.:?!) v k = v .: k .!= ""
-
-{- 
--- ---------------------------------------------------------------------------------------
-jsonFile :: FilePath
--- jsonFile = "bern.json"    -- use datatype  IO (Either String SmnRecord)
-jsonFile = "all.json"     -- use datatype  IO (Either String [SmnRecord])
-
-getJSON :: IO B.ByteString
-getJSON = B.readFile jsonFile
-
-main :: IO ()
-main = do
- -- Get JSON data and decode it
- d <- (eitherDecode <$> getJSON) :: IO (Either String [SmnRecord])
- -- If d is Left, the JSON was malformed.
- -- In that case, we report the error.
- -- Otherwise, we perform the operation of
- -- our choice. In this case, just print it.
- case d of
-   Left err -> putStrLn err
-   Right ps -> print ps
-   -- Right ps -> print (map (getStationName . smnStation) ps)
-
-getStationName :: Maybe Station -> T.Text
-getStationName (Just starec) = staName starec
-getStationName Nothing = "STATION IS NOTHING"
-
--}
